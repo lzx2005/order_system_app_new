0ef05f7a7a5e2081e031d8f23eda63beaced79a6
lzx2005 <978744976@qq.com>
2017-05-08 13:49:46 +0800

    点菜模块

--- app/src/main/java/com/lzx2005/system/order/activity/MenuActivity.java
+++ app/src/main/java/com/lzx2005/system/order/activity/MenuActivity.java
@@ -1,30 +1,74 @@
 package com.lzx2005.system.order.activity;
 
+import android.content.SharedPreferences;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.SimpleAdapter;
 
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
 import com.lzx2005.system.order.R;
+import com.lzx2005.system.order.http.task.GetTask;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 public class MenuActivity extends AppCompatActivity {
 
-    private String[] data = {"wt","t1","teaasda"};
+    SharedPreferences loginInfo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+        setTitle("点菜");
+        loginInfo = getSharedPreferences("loginInfo", 0);
 
-        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(MenuActivity.this,R.layout.menu_list_item,data);
+        String token = loginInfo.getString("token", "");
+        String restaurantId = this.getIntent().getExtras().getString("restaurantId");
+        String host = getResources().getString(R.string.server_host);
+        String url = host + "/rest/dish/getByRestId?token="+token+"&restId="+restaurantId;
 
-        ListView listView = (ListView) findViewById(R.id.menuList);
-        listView.setAdapter(arrayAdapter);
-
-        listView.setOnItemClickListener((parent, view, position, id) -> {
-            //todo 对每次点击进行添加到"购物篮"操作
-        });
+        GetTask getTask = new GetTask(url, showMenuHandler);
+        new Thread(getTask).start();
     }
+
+
+    Handler showMenuHandler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            Bundle data = msg.getData();
+            String val = data.getString("value");
+            Log.i("lzx", val);
+            JSONObject result = JSONObject.parseObject(val);
+            JSONObject root = result.getJSONObject("data");
+            List<HashMap<String,Object>> list = new ArrayList<>();
+            for(String key : root.keySet()){
+                JSONArray array = root.getJSONArray(key);
+                for(int i=0;i<array.size();i++){
+                    JSONObject dish = array.getJSONObject(i);
+
+                    HashMap<String,Object> hashMap = new HashMap<>();
+                    hashMap.put("name",dish.getString("name"));
+                    hashMap.put("price",dish.getString("price"));
+                    list.add(hashMap);
+                }
+            }
+
+            SimpleAdapter simpleAdapter = new SimpleAdapter(MenuActivity.this,
+                    list,
+                    R.layout.menu_list_item,
+                    new String[]{"name", "price"},
+                    new int[]{R.id.dish_name, R.id.dish_price});
+            ListView listView = (ListView) findViewById(R.id.menu_list);
+            listView.setAdapter(simpleAdapter);
+        }
+    };
+
 }
