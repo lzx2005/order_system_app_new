ef677e31386528a881b36ca35b1ef753b122e8f9
lzx2005 <978744976@qq.com>
2017-05-13 20:15:44 +0800

    首页显示附近的餐厅

--- app/src/main/java/com/lzx2005/system/order/activity/MainActivity.java
+++ app/src/main/java/com/lzx2005/system/order/activity/MainActivity.java
@@ -1,5 +1,6 @@
 package com.lzx2005.system.order.activity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
@@ -9,7 +10,6 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.text.TextUtils;
 import android.util.Log;
-import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -18,21 +18,40 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
+import com.amap.api.location.AMapLocation;
+import com.amap.api.location.AMapLocationClient;
+import com.amap.api.location.AMapLocationClientOption;
+import com.amap.api.location.AMapLocationListener;
+import com.amap.api.maps.model.LatLng;
+import com.amap.api.maps.model.Marker;
+import com.amap.api.maps.model.MarkerOptions;
 import com.lzx2005.system.order.R;
-import com.lzx2005.system.order.http.task.GetTask;
+import com.lzx2005.system.order.http.task.GetNearRestautantTask;
 import com.lzx2005.system.order.http.task.GetUserInfoTask;
-import com.lzx2005.system.order.http.task.LoginTask;
 
-import java.util.Map;
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
-public class MainActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+
+public class MainActivity
+        extends
+            AppCompatActivity
+        implements
+            NavigationView.OnNavigationItemSelectedListener,
+            AMapLocationListener, AdapterView.OnItemClickListener{
 
     SharedPreferences loginInfo;
 
@@ -52,15 +71,27 @@ public class MainActivity extends AppCompatActivity
     Menu slideMenu;
     MenuItem navExitMenuItem;
 
+    ListView restList;
+    //声明AMapLocationClient类对象
+    public AMapLocationClient mLocationClient = null;
+
+    //声明mLocationOption对象
+    public AMapLocationClientOption mLocationOption = null;
+
+    List<HashMap<String,Object>> restDataList = new ArrayList<>();
+
+
+    private ProgressDialog progressDialog;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         loginInfo = getSharedPreferences("loginInfo", 0);
         setContentView(R.layout.activity_main);
+        progressDialog = ProgressDialog.show(MainActivity.this, "正在查找附近的餐厅...", "请稍后...", true, false);
         loadView();
         setSupportActionBar(toolbar);
         fab.setOnClickListener(view -> Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("知道了", v -> Log.e("lzx2005","1")).show());
+                .setAction("知道了", v -> Log.e("lzx2005", "1")).show());
 
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
@@ -70,21 +101,24 @@ public class MainActivity extends AppCompatActivity
 
         navigationView.setNavigationItemSelectedListener(this);
 
-        String token = loginInfo.getString("token","no");//找到登录信息
-        if(token.equals("no")){
+        String token = loginInfo.getString("token", "no");//找到登录信息
+        if (token.equals("no")) {
             //未登录
             navExitMenuItem.setVisible(false);
-            linearLayout.setOnClickListener(v->{
-                Intent intent = new Intent(MainActivity.this,LoginActivity.class);
-                startActivityForResult(intent,1);
+            linearLayout.setOnClickListener(v -> {
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivityForResult(intent, 1);
             });
-        }else{
-            Log.i("lzx","已经登录，从服务器获取信息");
+        } else {
+            Log.i("lzx", "已经登录，从服务器获取信息");
             String host = getResources().getString(R.string.server_host);
-            String url = host + "/user/userInfo?token="+token;
+            String url = host + "/user/userInfo?token=" + token;
             GetUserInfoTask getUserInfoTask = new GetUserInfoTask(url, handler);
             new Thread(getUserInfoTask).start();
+
         }
+
+
     }
 
     @Override
@@ -190,6 +224,63 @@ public class MainActivity extends AppCompatActivity
     };
 
 
+
+    Handler showNearRestauranthandler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            Bundle data = msg.getData();
+            String val = data.getString("value");
+            Log.i("lzx", val);
+            JSONObject jsonObject = JSONObject.parseObject(val);
+            if(jsonObject==null){
+                Log.e(this.getClass().getName(),"JSON渲染错误");
+            }else if(jsonObject.getInteger("code")==0){
+                JSONObject data1 = jsonObject.getJSONObject("data");
+                JSONArray content = data1.getJSONArray("content");
+
+
+                for(int i =0;i<content.size();i++){
+                    JSONObject re = content.getJSONObject(i);
+                    JSONObject distance = re.getJSONObject("distance");
+                    Double value = distance.getDouble("value");
+                    String dis;
+                    DecimalFormat df = new DecimalFormat("######0.00");
+                    if(value<1.0){
+                        value = value*1000;
+                        dis = df.format(value);
+                        dis = dis+"米";
+                    }else{
+                        dis = df.format(value);
+                        dis = dis + "千米";
+                    }
+                    JSONObject restaurant = re.getJSONObject("content");
+                    HashMap<String, Object> hashMap = new HashMap<>();
+                    hashMap.put("restName",restaurant.getString("restaurantName"));
+                    hashMap.put("restDis","距您"+dis);
+                    hashMap.put("restaurantId",restaurant.getString("restaurantId"));
+                    restDataList.add(hashMap);
+                    Log.i("lzx",re.toJSONString());
+                    //JSONArray position = restaurant.getJSONArray("position");
+                }
+
+                SimpleAdapter simpleAdapter = new SimpleAdapter(
+                        MainActivity.this,
+                        restDataList,
+                        R.layout.rest_list_item,
+                        new String[]{"restName","restDis"},
+                        new int[]{R.id.rest_name,R.id.rest_dis}
+                );
+                restList.setAdapter(simpleAdapter);
+                restList.setOnItemClickListener(MainActivity.this);
+                progressDialog.dismiss();
+            }else{
+                String msg1 = jsonObject.getString("msg");
+                Log.e(this.getClass().getName(),msg1);
+                Toast.makeText(MainActivity.this,msg1,Toast.LENGTH_SHORT).show();
+                progressDialog.dismiss();
+            }
+        }
+    };
     private void loadView(){
         toolbar = (Toolbar) findViewById(R.id.toolbar);
         fab = (FloatingActionButton) findViewById(R.id.fab);
@@ -206,5 +297,53 @@ public class MainActivity extends AppCompatActivity
         slideMenu = navigationView.getMenu();
         navExitMenuItem = slideMenu.findItem(R.id.nav_exit);
 
+        restList = (ListView) findViewById(R.id.rest_list);
+        //初始化定位
+        mLocationClient = new AMapLocationClient(getApplicationContext());
+        //设置定位回调监听
+        mLocationClient.setLocationListener(this);
+        mLocationOption = new AMapLocationClientOption();
+        mLocationOption.setLocationMode(AMapLocationClientOption.AMapLocationMode.Hight_Accuracy);
+
+        //获取一次定位结果：
+        //该方法默认为false。
+        mLocationOption.setOnceLocation(true);
+
+        //获取最近3s内精度最高的一次定位结果：
+        //设置setOnceLocationLatest(boolean b)接口为true，启动定位时SDK会返回最近3s内精度最高的一次定位结果。如果设置其为true，setOnceLocation(boolean b)接口也会被设置为true，反之不会，默认为false。
+        mLocationOption.setOnceLocationLatest(true);
+
+
+        //给定位客户端对象设置定位参数
+        mLocationClient.setLocationOption(mLocationOption);
+        //启动定位
+        mLocationClient.startLocation();
+
+    }
+
+    @Override
+    public void onLocationChanged(AMapLocation aMapLocation) {
+        double longitude = aMapLocation.getLongitude();
+        double latitude = aMapLocation.getLatitude();
+        Log.i("lzx","lng:"+longitude+",lat:"+latitude);
+
+
+
+        String token = loginInfo.getString("token", "no");//找到登录信息
+        if(token.equals("no")){
+            Toast.makeText(this,"暂未登录",Toast.LENGTH_SHORT).show();
+            return;
+        }
+        String host = getResources().getString(R.string.server_host);
+        String url = host + "/rest/restaurant/near?token="+token+"&lng="+longitude+"&lat="+latitude+"&length="+10;
+        GetNearRestautantTask getUserInfoTask = new GetNearRestautantTask(url, showNearRestauranthandler);
+        new Thread(getUserInfoTask).start();
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        Intent intent = new Intent(MainActivity.this,MenuActivity.class);
+        intent.putExtra("restaurantId",(String)restDataList.get(position).get("restaurantId"));
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/lzx2005/system/order/activity/MenuActivity.java
+++ app/src/main/java/com/lzx2005/system/order/activity/MenuActivity.java
@@ -1,5 +1,6 @@
 package com.lzx2005.system.order.activity;
 
+import android.app.ProgressDialog;
 import android.content.SharedPreferences;
 import android.os.Handler;
 import android.os.Message;
@@ -27,52 +28,27 @@ public class MenuActivity extends AppCompatActivity implements MenuAdapter.OnIte
     List<HashMap<String,Object>> list;
     MenuAdapter menuAdapter;
     TextView totalPriceView;
+
+    private ProgressDialog progressDialog;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
         setTitle("点菜");
+
+        progressDialog = ProgressDialog.show(MenuActivity.this, "正在加载菜单...", "请稍后...", true, false);
         loginInfo = getSharedPreferences("loginInfo", 0);
         list = new ArrayList<>();
         totalPriceView = (TextView) findViewById(R.id.total_price);
-        menuAdapter = new MenuAdapter(this, (viewHolder, clickView, position) -> {
-            HashMap<String, Object> hashMap = list.get(position);
-            int count = (int) hashMap.get("count");
-            switch (clickView.getId()){
-                case R.id.dish_minus_btn:
-                    Log.i("lzx",viewHolder.dishName.getText().toString()+"减少一个");
-                    if(count>0){
-                        count -= 1;
-                    }
-                    break;
-                case R.id.dish_add_btn:
-                    Log.i("lzx",viewHolder.dishName.getText().toString()+"增加一个");
-                    count += 1;
-                    break;
-            }
-            if(count<0){
-                count=0;
-            }
-            hashMap.put("count",count);
-            viewHolder.sum.setText(count+"");
-
-            //计算总价
-            double totalPrice = 0.0;
-            for(HashMap<String,Object> map : list){
-                int count1 = (int) map.get("count");
-                String priceStr1 = (String) map.get("price");
-                double price1 = Double.parseDouble(priceStr1);
-                totalPrice = totalPrice + (count1*price1);
-            }
-            DecimalFormat df = new DecimalFormat("######0.00");
-            totalPriceView.setText("总价："+df.format(totalPrice)+"元");
-        });
+        menuAdapter = new MenuAdapter(this, this);
         String token = loginInfo.getString("token", "");
         String restaurantId = this.getIntent().getExtras().getString("restaurantId");
         String host = getResources().getString(R.string.server_host);
         String url = host + "/rest/dish/getByRestId?token="+token+"&restId="+restaurantId;
 
         GetTask getTask = new GetTask(url, showMenuHandler);
+
         new Thread(getTask).start();
     }
 
@@ -100,12 +76,12 @@ public class MenuActivity extends AppCompatActivity implements MenuAdapter.OnIte
             ListView listView = (ListView) findViewById(R.id.menu_list);
             menuAdapter.setList(list);
             listView.setAdapter(menuAdapter);
+            progressDialog.dismiss();
         }
     };
 
     @Override
     public void click(MenuAdapter.ViewHolder viewHolder, View clickView, int position) {
-        CharSequence priceStr = viewHolder.dishPrice.getText();
         HashMap<String, Object> hashMap = list.get(position);
         int count = (int) hashMap.get("count");
         switch (clickView.getId()){
